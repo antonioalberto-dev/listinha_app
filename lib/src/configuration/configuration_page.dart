32fb5e96e1f6dca80ca508da3c74b295443d8305
Antonio Alberto <antonioalbertodsb@gmail.com>
2024-04-02 21:56:45 -0400

    feat: mudando tema do app por meio das configuracoes

--- lib/src/configuration/configuration_page.dart
+++ lib/src/configuration/configuration_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:listinha/src/shared/store/app_store.dart';
 
 class ConfigurationPage extends StatefulWidget {
   const ConfigurationPage({super.key});
@@ -10,9 +12,60 @@ class ConfigurationPage extends StatefulWidget {
 class _ConfigurationPageState extends State<ConfigurationPage> {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final appStore = context.watch<AppStore>(
+      (store) => store.themeMode,
+    );
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Configurações'),
+        title: const Text('LISTINHA'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: textTheme.titleLarge,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Tema',
+              style: textTheme.titleMedium,
+            ),
+            RadioListTile(
+              value: ThemeMode.system,
+              groupValue: appStore.themeMode.value,
+              title: const Text('Sistema'),
+              onChanged: appStore.changeThemeMode,
+            ),
+            RadioListTile(
+              value: ThemeMode.light,
+              groupValue: appStore.themeMode.value,
+              title: const Text('Light'),
+              onChanged: appStore.changeThemeMode,
+            ),
+            RadioListTile(
+                value: ThemeMode.dark,
+                groupValue: appStore.themeMode.value,
+                title: const Text('Dark'),
+                onChanged: appStore.changeThemeMode),
+            const SizedBox(height: 20),
+            Text(
+              'Controle de dados',
+              style: textTheme.titleMedium,
+            ),
+            const SizedBox(height: 15),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                'Apagar cache e reiniciar app',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
